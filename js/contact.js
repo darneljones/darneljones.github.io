3e9ca463c9696cdd0d6457d8c01d19864849ee8a
Darnel Jones <darneljones.corp@gmail.com>
2020-12-30 16:03:59 -0500

    Revert "One Big Commit"
    This reverts commit e92bc4b104b1abd6adfa647b13ada099767ebd0b.

--- js/contact.js
+++ js/contact.js
@@ -1,53 +1,47 @@
 $(document).ready(function () {
-  // Test for placeholder support
-  $.support.placeholder = (function () {
-    var i = document.createElement("input");
-    return "placeholder" in i;
-  })();
+    // Test for placeholder support
+    $.support.placeholder = (function () {
+        var i = document.createElement('input');
+        return 'placeholder' in i;
+    })();
 
-  // Hide labels by default if placeholders are supported
-  if ($.support.placeholder) {
-    $(".form-label").each(function () {
-      $(this).addClass("js-hide-label");
-    });
+    // Hide labels by default if placeholders are supported
+    if ($.support.placeholder) {
+        $('.form-label').each(function () {
+            $(this).addClass('js-hide-label');
+        });
 
-    // Code for adding/removing classes here
-    $(".form-group")
-      .find("input, textarea")
-      .on("keyup blur focus", function (e) {
-        // Cache our selectors
-        var $this = $(this),
-          $parent = $this.parent().find("label");
+        // Code for adding/removing classes here
+        $('.form-group').find('input, textarea').on('keyup blur focus', function (e) {
 
-        switch (e.type) {
-          case "keyup":
-            {
-              $parent.toggleClass("js-hide-label", $this.val() == "");
+            // Cache our selectors
+            var $this = $(this),
+                $parent = $this.parent().find("label");
+
+            switch (e.type) {
+                case 'keyup': {
+                    $parent.toggleClass('js-hide-label', $this.val() == '');
+                }
+                    break;
+                case 'blur': {
+                    if ($this.val() == '') {
+                        $parent.addClass('js-hide-label');
+                    } else {
+                        $parent.removeClass('js-hide-label').addClass('js-unhighlight-label');
+                    }
+                }
+                    break;
+                case 'focus': {
+                    if ($this.val() !== '') {
+                        $parent.removeClass('js-unhighlight-label');
+                    }
+                }
+                    break;
+                default:
+                    break;
             }
-            break;
-          case "blur":
-            {
-              if ($this.val() == "") {
-                $parent.addClass("js-hide-label");
-              } else {
-                $parent
-                  .removeClass("js-hide-label")
-                  .addClass("js-unhighlight-label");
-              }
-            }
-            break;
-          case "focus":
-            {
-              if ($this.val() !== "") {
-                $parent.removeClass("js-unhighlight-label");
-              }
-            }
-            break;
-          default:
-            break;
-        }
-        // previous implementation with ifs
-        /*if (e.type == 'keyup') {
+            // previous implementation with ifs
+            /*if (e.type == 'keyup') {
              if( $this.val() == '' ) {
              $parent.addClass('js-hide-label');
              } else {
@@ -67,6 +61,6 @@ $(document).ready(function () {
              $parent.removeClass('js-unhighlight-label');
              }
              }*/
-      });
-  }
+        });
+    }
 });
